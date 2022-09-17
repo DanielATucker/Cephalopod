dea57ce0adda6b6d0e6f2d1a08eedf716966d87a
Daniel <DanielATucker@Outlook.com>
2022-09-16 20:01:24 -0400

    Updated create

--- modules/Brain_handlers/journal_handler.py
+++ modules/Brain_handlers/journal_handler.py
@@ -3,27 +3,37 @@ from ..Private_Message import Private_Message
 
 class journal_handler:
 
-    def __init__(self, sio):
+    def __init__(self, sio, username, User_list):
 
-        class Events:
+        self.init1(sio)
 
-            def __init__(self, sio):
-
-                @sio.event
-                def body(self, journal_body):
-
-                    sio.emit("private_message", f"{journal_body}")
-
-        Events = Events(sio)
-        self.body = Events.body()
+        self.username = username
+        self.User_list = User_list
 
 
-    def title(self, sio, user_id, journal_title, User_list):
+    def init1(self, sio):
 
-        print(f"user id {user_id}")
-        Private_Message(f"Let's create a new Jounal entry. Journal title: {journal_title}. Journal body?", user_id, sio, User_list)
+        @sio.event
+        def body(self, journal_body):
+
+            username = self.username
+            User_list = self.User_list
+
+            Private_message(f"{journal_body}", username, sio, User_list)
+            print("sent")
 
 
-    def Etitle(self, sio, user_id, journal_title, journal_body, mood, anxiety, depression, energy):
+        @sio.event
+        def title(self, journal_title):
+
+            username = self.username
+            User_list = self.User_list
+
+            print(f"user id {user_id}")
+
+            Private_Message(f"Let's create a new Jounal entry. Journal title: {journal_title}. Journal body?", username, sio, User_list)
+
+
+    def Etitle(self, sio, username, User_list, journal_title, journal_body, mood, anxiety, depression, energy):
 
         sio.emit (f"You have already created a journal today, here it is\n Journal title: {journal_title}\n Body: {journal_body}\n Mood: {mood}\n Anxiety: {anxiety}\n Depression: {depression}\n Energy: {energy}\n", user_id)
--- modules/create.py
+++ modules/create.py
@@ -4,11 +4,13 @@ import inspect
 
 # modules
 
+
 from .Brain_handlers.journal_handler import journal_handler
+
 
 def main(log, graph, journal_title, date_format, sio, user_id, username, User_list):
 
-    Journal_handler = journal_handler(sio)
+    Journal_handler = journal_handler(sio, username, User_list)
 
     is_journal_created_today = graph.run(f"MATCH (j: Journal), (u: User), (J: JournalMaster), (j: Journal), (j)-[*]->(J)-[r: link]->(u) WHERE j.name = '{journal_title}' AND u.name = '{username}' RETURN j.is_journal_created_today ", journal_title=journal_title, username=username).evaluate()
 
@@ -40,9 +42,9 @@ def main(log, graph, journal_title, date_format, sio, user_id, username, User_li
 
         graph.run(f"MATCH (u: User), (J: JournalMaster), (J)-[s: link]->(u) WHERE u.name = '{username}' CREATE (j: Journal)-[r: Journal_of]->(J) SET j.name = '{journal_title}', j.is_journal_created_today = 1", journal_title=journal_title, username=username)
 
-        Journal_handler.title(sio, user_id, journal_title, User_list)
+        Journal_handler.init1.title(username, User_list, journal_title)
 
-        Journal_handler.body(sio, user_id, journal_body, User_list)
+        Journal_handler.init1.body(username, User_list, journal_body)
 
 
         graph.run(f"MATCH (u: User), (J: JournalMaster), (j: Journal), (j)-[*]->(J)-[r: link]->(u) WHERE u.name = '{username}' AND j.name = '{journal_title}' SET j.name = '{journal_title}', j.body = '{journal_body}' ", journal_title=journal_title, journal_body=journal_body)
