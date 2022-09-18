da62211c0fa08f7e5f8696e5ccd853b2a9148fbe
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-09-18 02:53:57 -0400

    Updated create

--- modules/create.py
+++ modules/create.py
@@ -10,7 +10,7 @@ from .Brain_handlers.journal_handler import journal_handler
 
 def main(log, graph, journal_title, date_format, sio, user_id, username, User_list):
 
-    journal_handler = journal_handler(journal_title, date_format, sio, user_id, username, User_list)
+    Journal_handler = journal_handler(journal_title, date_format, sio, user_id, username, User_list)
 
 
     is_journal_created_today = graph.run(f"MATCH (j: Journal), (u: User), (J: JournalMaster), (j: Journal), (j)-[*]->(J)-[r: link]->(u) WHERE j.name = '{journal_title}' AND u.name = '{username}' RETURN j.is_journal_created_today ", journal_title=journal_title, username=username).evaluate()
