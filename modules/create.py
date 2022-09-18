61a5168a45604a2ca73959aa1761152779a89c65
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-09-18 04:10:30 -0400

    updated create

--- modules/create.py
+++ modules/create.py
@@ -2,10 +2,36 @@ import os
 import sys
 import inspect
 
-# modules
+
+class journal_handler(sio):
+
+    def __init__(self, sio):
+        sio = sio
 
 
-from .Brain_handlers.journal_handler import journal_handler
+    @sio.event
+    def body(username, User_list, journal_body):
+
+        Private_Message(f"{journal_body}", username, sio, User_list)
+        print("sent")
+
+    
+
+
+    @sio.event
+    def title(username, User_list, journal_title):
+        
+        print(f"username {username}")
+
+        Private_Message(f"Let's create a new Jounal entry. Journal title: {journal_title}. Journal body?", username, sio, User_list)
+
+    
+
+
+    @sio.event
+    def Etitle(username, User_list, journal_title, journal_body, mood, anxiety, depression, energy):
+
+        sio.emit (f"You have already created a journal today, here it is\n Journal title: {journal_title}\n Body: {journal_body}\n Mood: {mood}\n Anxiety: {anxiety}\n Depression: {depression}\n Energy: {energy}\n", user_id)
 
 
 def main(log, graph, journal_title, date_format, sio, user_id, username, User_list):
