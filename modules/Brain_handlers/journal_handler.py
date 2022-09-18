cf7befc3f88d53ddf21bdc809545830d79b63b55
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-09-18 02:45:26 -0400

    Updated journal_handler

--- modules/Brain_handlers/journal_handler.py
+++ modules/Brain_handlers/journal_handler.py
@@ -9,6 +9,8 @@ def journal_handler(journal_title, date_format, sio, user_id, username, User_lis
         Private_Message(f"{journal_body}", username, sio, User_list)
         print("sent")
 
+        return body
+
 
     @sio.event
     def title(username, User_list, journal_title):
@@ -17,7 +19,12 @@ def journal_handler(journal_title, date_format, sio, user_id, username, User_lis
 
         Private_Message(f"Let's create a new Jounal entry. Journal title: {journal_title}. Journal body?", username, sio, User_list)
 
+        return title
+
+
     @sio.event
     def Etitle(username, User_list, journal_title, journal_body, mood, anxiety, depression, energy):
 
         sio.emit (f"You have already created a journal today, here it is\n Journal title: {journal_title}\n Body: {journal_body}\n Mood: {mood}\n Anxiety: {anxiety}\n Depression: {depression}\n Energy: {energy}\n", user_id)
+
+        return Etitle
