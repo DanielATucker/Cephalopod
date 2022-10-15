8e7228e22def1d9b5b6a660f7a8fe5ae4321a15c
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-10-15 04:02:08 -0400

    Updated create

--- modules/create.py
+++ modules/create.py
@@ -1,9 +1,30 @@
 from .Private_Message import Private_Message
 
-from modules.Brain_handlers.journal_handler import journal_handler
 
 def main(log, graph, journal_title, date_format, sio, user_id, username):
-    
+         
+    @sio.event
+    def body(username,journal_body):
+            
+        Private_Message(f"{journal_body}", username, sio, "Brain")
+            
+        print("sent")
+
+
+    @sio.event
+    def title(username, journal_title):
+        
+        print(f"username {username}")
+            
+        Private_Message(f"Let's create a new Jounal entry. Journal title: {journal_title}. Journal body?", username, sio, "Brain")
+
+
+    @sio.event
+    def Etitle(username, journal_title, journal_body, mood, anxiety, depression, energy):
+            
+        Private_Message(f"You have already created a journal today, here it is\n Journal title: {journal_title}\n Body: {journal_body}\n Mood: {mood}\n Anxiety: {anxiety}\n Depression: {depression}\n Energy: {energy}\n", username, sio, "Brain")
+
+
     is_journal_created_today = graph.run(f"MATCH (j: Journal), (u: User), (J: JournalMaster), (j: Journal), (j)-[*]->(J)-[r: link]->(u) WHERE j.name = '{journal_title}' AND u.name = '{username}' RETURN j.is_journal_created_today ", journal_title=journal_title, username=username).evaluate()
 
     if is_journal_created_today == 1:
@@ -34,6 +55,8 @@ def main(log, graph, journal_title, date_format, sio, user_id, username):
         graph.run(f"MATCH (u: User), (J: JournalMaster), (J)-[s: link]->(u) WHERE u.name = '{username}' CREATE (j: Journal)-[r: Journal_of]->(J) SET j.name = '{journal_title}', j.is_journal_created_today = 1", journal_title=journal_title, username=username)
 
         title(username, journal_title)
+
+        body(username, journal_body)
 
 
         graph.run(f"MATCH (u: User), (J: JournalMaster), (j: Journal), (j)-[*]->(J)-[r: link]->(u) WHERE u.name = '{username}' AND j.name = '{journal_title}' SET j.name = '{journal_title}', j.body = '{journal_body}' ", journal_title=journal_title, journal_body=journal_body)
