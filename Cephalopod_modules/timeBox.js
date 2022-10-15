78eb1cb194dba2ac2c9f4393a2144b813d9b97ba
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-10-15 19:14:44 -0400

    Updated timebox

--- Cephalopod_modules/timeBox.js
+++ Cephalopod_modules/timeBox.js
@@ -9,30 +9,7 @@ var blessed = require('blessed')
 export default function timebox(grid, screen) {
   setInterval(()=> {
 
-    let date_ob = new Date();
-
-    // current date
-    // adjust 0 before single digit date
-    let date = ("0" + date_ob.getDate()).slice(-2);
-
-    // current month
-    let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
-
-    // current year
-    let year = date_ob.getFullYear();
-
-    // current hours
-    let hours = date_ob.getHours();
-
-    // current minutes
-    let minutes = date_ob.getMinutes();
-
-    // current seconds
-    let seconds = date_ob.getSeconds();
-
-    // prints date in YYYY-MM-DD format
-    let Now = (year + month + date + " " + hours + ":" + minutes +  "." + seconds);
-
+    let Now = strftime('%y%m%d %X', new Date(1307472705067))
 
     var TimeBox = grid.set(0, 0, 2, 2, blessed.box, {
       width: '100%',
