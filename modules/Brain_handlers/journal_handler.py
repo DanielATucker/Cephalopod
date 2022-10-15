9ddbf0d1e4874f4e7c6d9ecaa1e95f22e56f3d91
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-10-15 04:06:02 -0400

    Updated create and journal_handler

--- modules/Brain_handlers/journal_handler.py
+++ modules/Brain_handlers/journal_handler.py
@@ -1,28 +1,24 @@
 from ..Private_Message import Private_Message
 import sys
-from ...Cephalopod import sio
+
 class journal_handler:
 
     @sio.event
-    def body(username, User_list, journal_body):
+    def body(sio, username, User_list, journal_body):
 
         Private_Message(f"{journal_body}", username, sio, User_list)
         print("sent")
 
     
-
-
     @sio.event
-    def title(username, User_list, journal_title):
+    def title(sio, username, User_list, journal_title):
         
         print(f"username {username}")
 
         Private_Message(f"Let's create a new Jounal entry. Journal title: {journal_title}. Journal body?", username, sio, User_list)
 
     
-
-
     @sio.event
-    def Etitle(username, User_list, journal_title, journal_body, mood, anxiety, depression, energy):
+    def Etitle(sio, username, User_list, journal_title, journal_body, mood, anxiety, depression, energy):
 
         sio.emit (f"You have already created a journal today, here it is\n Journal title: {journal_title}\n Body: {journal_body}\n Mood: {mood}\n Anxiety: {anxiety}\n Depression: {depression}\n Energy: {energy}\n", user_id)
