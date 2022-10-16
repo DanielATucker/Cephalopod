52e905b955cb9ec9a5bd5bd5878426bd2b6f8c2f
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-10-16 09:52:57 -0400

    update terminalBox.js

--- Cephalopod_modules/terminalBox.js
+++ Cephalopod_modules/terminalBox.js
@@ -10,36 +10,6 @@ export default function terminalbox(grid, screen) {
   let Terminal = null;
   let terminalContent = "terminal WILL GO HERE";
 
-  function refresh(screen) { 
-    setInterval(()=> {
-      var terminalBox = grid.set(5, 0, 3, 8, blessed.box({
-        width: '100%',
-        height: '100%',
-        content: terminalContent,
-        tags: true,
-        border: {
-          type: 'line'
-        },
-        style: {
-          fg: 'red',
-          bg: 'black',
-          border: {
-            fg: 'red'
-          },
-          hover: {
-            bg: 'black'
-          }
-        },
-      }));
-
-      Terminal.focus();
-
-      screen.render();
-    });
-
-    refresh();
-
-  /*
   refresh(get_Terminal);
 
   function get_Terminal(TerminalIn){
@@ -57,6 +27,4 @@ export default function terminalbox(grid, screen) {
       callback(Terminal);
     });
   }
-  */
- }
-};
+ };
