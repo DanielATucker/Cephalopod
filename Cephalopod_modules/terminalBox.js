ab3d920667ebb647955069e831c8a9863b99779f
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-10-16 11:07:55 -0400

    update terminalBox.js

--- Cephalopod_modules/terminalBox.js
+++ Cephalopod_modules/terminalBox.js
@@ -14,7 +14,7 @@ export default function terminalbox(grid, screen) {
 
   function refresh() { 
     setInterval(()=> {
-      var Terminal = grid.set(5, 0, 3, 8, blessed.textbox({
+      var Terminal = grid.set(5, 0, 3, 8, blessed.textbox, ({
         top: 4,
         left: 5,
         height: 3,
