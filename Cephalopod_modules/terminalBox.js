1a987cc571407caca33165f1c90cc59fe636fc38
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-10-16 10:05:21 -0400

    update terminalBox.js

--- Cephalopod_modules/terminalBox.js
+++ Cephalopod_modules/terminalBox.js
@@ -8,23 +8,46 @@ var contrib = require('blessed-contrib')
 
 export default function terminalbox(grid, screen) {
   let Terminal = null;
+  let text = null;
   let terminalContent = "terminal WILL GO HERE";
 
   refresh(get_Terminal);
 
-  function get_Terminal(TerminalIn){
+  function get_Terminal(TerminalIn, textIn){
     Terminal = TerminalIn
+    text = textIn
   }
 
   function refresh(callback) { 
     setInterval(()=> {
-      var Terminal = grid.set(5, 0, 3, 8, contrib.log,({ fg: "red", selectedFg: "red", label: 'Terminal'}));
+      var TerminalIn = grid.set(6, 0, 3, 8, blessed.textarea, ({
+        parent: screen,
+        width: '100%',
+        height: '100%',
+        border: {
+          type: 'line'
+        },
+        inputOnFocus: true,
+        style: {
+          border: {
+            fg: 'red'
+          }
+        }
+      }));
 
-      Terminal.log(terminalContent);
+      input.key(['enter'], () => input.submit());
+      input.key(['escape'], () => input.cancel());
+      input.on('submit', () => {
+        let text = input.getValue();
 
-      screen.render();
-    
-      callback(Terminal);
+        callback(TerminalIn, text)
+
+        input.destroy();
+      });
+      input.on('cancel', () => {
+        input.destroy();
+      });
+      input.focus();
     });
   }
- };
+};
