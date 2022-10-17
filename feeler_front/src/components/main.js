b855cf68d8f02bbf25b34e84f99cc2356e4bcbe7
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-10-17 17:29:03 -0400

    update main.js

--- feeler_front/src/components/main.js
+++ feeler_front/src/components/main.js
@@ -7,11 +7,13 @@ class Main extends Component{
     constructor(props) {
         super(props);
 
+        this.username = "No user logged in"
+
         this.get_username();
     }
 
     async get_username(){
-        const response = await fetch('100.69.19.3:3001/get_username');
+        const response = await fetch('http://100.69.19.3:3001/get_username');
         const username = await response.json();
       
         this.username = username;
