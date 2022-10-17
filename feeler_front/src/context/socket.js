522dbf2eb6ac6beed8c2033bdf9f14e930947471
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-10-17 16:18:53 -0400

    update socket.js

--- feeler_front/src/context/socket.js
+++ feeler_front/src/context/socket.js
@@ -1,5 +1,5 @@
 import React from "react";
 import socketio from "socket.io-client";
 
-export const socket = socketio.connect("http://localhost:3000");
+export const socket = socketio.connect("ws://localhost:3000");
 export const SocketContext = React.createContext();
