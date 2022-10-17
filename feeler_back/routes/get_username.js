74d51e69f6fc85c2fdba100ee07bf0349b4ed18d
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-10-17 17:34:03 -0400

    update get_username.js

--- feeler_back/routes/get_username.js
+++ feeler_back/routes/get_username.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send({"USERNAME": "USERNAME"});
+  res.json({"USERNAME": "USERNAME"});
 });
 
 module.exports = router;
