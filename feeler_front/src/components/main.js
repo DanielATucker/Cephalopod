f01636d30f25a968db0ce3f10cc696f946be9e3a
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-10-19 05:53:05 -0400

    update main.js

--- feeler_front/src/components/main.js
+++ feeler_front/src/components/main.js
@@ -9,14 +9,14 @@ function Main () {
 
     //setUsername("No user logged in");
 
-    username = fetch('http://100.69.19.3:3001/get_username')
+    usernameIn = fetch('http://100.69.19.3:3001/get_username')
         .then((response) => response.json())
         .then((responseJSON) => {
         console.log(responseJSON);
     });
 
     useEffect(() => {
-        setUsername(username);
+        setUsername(usernameIn);
     });
 
     
