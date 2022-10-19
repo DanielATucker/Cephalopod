7ad7267ced4d801b62617d59ca8253367db8cc13
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-10-19 17:02:16 -0400

    update main.js

--- feeler_front/src/components/main.js
+++ feeler_front/src/components/main.js
@@ -10,7 +10,7 @@ export default class Main extends React.Component {
 
         this.state = {
             "username": "No user logged in",
-            "Terminal": Terminal()
+            "Terminal": new Terminal
         };
     };
     
