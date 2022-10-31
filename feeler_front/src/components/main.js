98218b1d925d644a44c2aa487fcc15dc4dd839c2
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-10-30 23:16:04 -0400

    update main.js

--- feeler_front/src/components/main.js
+++ feeler_front/src/components/main.js
@@ -22,7 +22,7 @@ export default class Main extends React.Component {
 
         setMessages(this.state.socketHandler.state.data.messages)
 
-        setSocketHandler(SocketHandler);
+        setSocketHandler(new SocketHandler);
 
 
         this.setState({
