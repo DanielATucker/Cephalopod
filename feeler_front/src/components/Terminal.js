347e549399a9e8b15f302279f847d4851747972b
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-10-31 19:33:28 -0400

    update Terminal.js

--- feeler_front/src/components/Terminal.js
+++ feeler_front/src/components/Terminal.js
@@ -1,11 +1,32 @@
 import React, { useEffect, useState } from "react";
-import { ScrollView, Text, View } from 'react';
+import { ScrollView, Text,  StyleSheet } from 'react';
 
 import Card from '@mui/material/Card';
 
 import SocketHandler from "./SocketHandler";
 
 export default function Terminal() {
+
+    const styles = StyleSheet.create({
+        paragraph: {
+          margin: 24,
+          fontSize: 18,
+          fontWeight: 'bold',
+          textAlign: 'center',
+        },
+        scrollView: {
+          height: '20%',
+          width: '80%',
+          margin: 20,
+          alignSelf: 'center',
+          padding: 20,
+          borderWidth: 5,
+          borderRadius: 5,
+          borderColor: 'black',
+          backgroundColor: 'lightblue'
+        }
+    });
+
     return (
         <>
         <Card variant="outlined">
