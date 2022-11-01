b799291411f60d3e109f6caf6ebe3c861e083a46
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-11-01 01:23:35 -0400

    update Terminal.js

--- feeler_front/src/components/Terminal.js
+++ feeler_front/src/components/Terminal.js
@@ -1,15 +1,35 @@
 import React, { useEffect, useState } from "react";
 import Card from '@mui/material/Card';
+import { StyleSheet, Text, SafeAreaView, ScrollView, StatusBar } from 'react-native';
 
-import SocketHandler from "./SocketHandler";
 
 export default function Terminal() {
+
+    const styles = StyleSheet.create({
+        container: {
+          flex: 1,
+          paddingTop: StatusBar.currentHeight,
+        },
+        scrollView: {
+          backgroundColor: 'pink',
+          marginHorizontal: 20,
+        },
+        text: {
+          fontSize: 42,
+        },
+    });
+
     return (
         <>
         <Card variant="outlined">
             <p> Terminal</p>
-        </Card> 
-        
+        </Card>
+         
+        <ScrollView style={styles.scrollView}>
+            <Text style={styles.text}>
+            </Text>
+        </ScrollView>
+
         <input></input>
         <button > Send </button>
         </>
