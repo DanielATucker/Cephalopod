f5af2358a71b2c8a082a148c120fd44a1ba4369c
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-11-01 01:49:53 -0400

    update Terminal.js

--- feeler_front/src/components/Terminal.js
+++ feeler_front/src/components/Terminal.js
@@ -9,7 +9,7 @@ export default class Terminal extends React.Component {
 
         //let messages = props.messages;
 
-        console.log(props.messages);
+        console.log(this.props);
         
         this.state = {
             "messages": "test"
