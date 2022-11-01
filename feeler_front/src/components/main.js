06de16d8541ac0772d117642a649f5002461060b
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-11-01 01:46:13 -0400

    update main.js

--- feeler_front/src/components/main.js
+++ feeler_front/src/components/main.js
@@ -31,7 +31,7 @@ export default class Main extends React.Component {
                 </body>
             </Card>
             
-            <Terminal> {this.state.messages} </Terminal>
+            <Terminal messsages={this.state.messages}> </Terminal>
             </>
         );
     };
