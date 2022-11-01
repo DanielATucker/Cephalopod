d3238b77085231bd05c98738070712bd32f77bab
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-11-01 14:51:32 -0400

    update main.js

--- feeler_front/src/components/main.js
+++ feeler_front/src/components/main.js
@@ -11,7 +11,7 @@ export default class Main extends React.Component {
 
         this.state = {
             "username": "No user logged in",
-            "messages": {}
+            "messages": []
         };
     };
 
