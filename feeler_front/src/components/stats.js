99e61ec9b5bca23449e04aed7d8c39c8c620edc3
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-11-11 00:37:19 -0500

    T update stats.js

--- feeler_front/src/components/stats.js
+++ feeler_front/src/components/stats.js
@@ -14,8 +14,12 @@ export default class Stats extends React.Component {
 
     render () {
         return(
+            <>
+            
             <h1> Stats </h1>
             <p> {this.state.stats}</p>
+
+            </>
         )
     }
 }
