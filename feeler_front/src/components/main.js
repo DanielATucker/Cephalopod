c501fc26bd7b848c5d626edcab6fb77b1202b5dc
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-11-12 23:30:37 -0500

    T update main.js

--- feeler_front/src/components/main.js
+++ feeler_front/src/components/main.js
@@ -27,7 +27,7 @@ export default class Main extends React.Component {
 
     handleStatsChange = (statsIn) => {
         this.setState({
-            "stats": statsIn
+            "stats": this.state.stats.concat(statsIn)
         });
 
         console.log(this.state.stats);
