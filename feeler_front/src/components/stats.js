fc4acc1dc6f9e9826e8201c9cab2b72402f52be7
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-11-18 14:04:56 -0500

    T update stats.js

--- feeler_front/src/components/stats.js
+++ feeler_front/src/components/stats.js
@@ -45,14 +45,14 @@ export default class Stats extends React.Component {
             "messages": [],
             "scrollviewMessages": [],
             "data": {
-                "CPU_Percent_list": [0],
-                "RAM_Percent_list": [0],
-                "Ram_GB_list": [0],
-                "Upload_list": [0],
-                "Upload_Speed_list": [0],
-                "Download_list": [0],
-                "Download_Speed_list": [0],
-                "Time_List": [0]
+                "CPU_Percent_list": [],
+                "RAM_Percent_list": [],
+                "Ram_GB_list": [],
+                "Upload_list": [],
+                "Upload_Speed_list": [],
+                "Download_list": [],
+                "Download_Speed_list": [],
+                "Time_List": []
             },
             "tables": {
                 "table1": table1,
@@ -72,20 +72,16 @@ export default class Stats extends React.Component {
             console.log(this.props.stats);
             console.log(typeof this.props.stats);
 
-            let CPU_Percent_Out = this.state.data.CPU_Percent_list.concat(this.props.stats.CPU_Percent)
-
-            console.log(CPU_Percent_Out);
-
             this.setState({            
                 "data": {
-                    "CPU_Percent_list": CPU_Percent_Out,
-                    "RAM_Percent_list": this.state.data.RAM_Percent_list.push(this.props.RAM_Percent),
-                    "Ram_GB_list": this.state.data.Ram_GB_list.push(this.props.Ram_GB),
-                    "Upload_list": this.state.data.Upload_list.push(this.props.Upload),
-                    "Upload_Speed_list": this.state.data.Upload_Speed_list.push(this.props.Upload_Speed),
-                    "Download_list": this.state.data.Download_list.push(this.props.Download),
-                    "Download_Speed_list": this.state.data.Download_Speed_list.push(this.props.Download_Speed),
-                    "Time_List": this.state.data.Time_List.push(this.props.Time)
+                    "CPU_Percent_list": this.state.data.CPU_Percent_list.concat(this.props.stats.CPU_Percent),
+                    "RAM_Percent_list": this.state.data.RAM_Percent_list.concat(this.props.RAM_Percent),
+                    "Ram_GB_list": this.state.data.Ram_GB_list.concat(this.props.Ram_GB),
+                    "Upload_list": this.state.data.Upload_list.concat(this.props.Upload),
+                    "Upload_Speed_list": this.state.data.Upload_Speed_list.concat(this.props.Upload_Speed),
+                    "Download_list": this.state.data.Download_list.concat(this.props.Download),
+                    "Download_Speed_list": this.state.data.Download_Speed_list.concat(this.props.Download_Speed),
+                    "Time_List": this.state.data.Time_List.concat(this.props.Time)
                 }
             });
 
