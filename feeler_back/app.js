50c638e28f4c19e06da1383e8286bca41c0aeced
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-11-20 15:42:43 -0500

    T update app.js

--- feeler_back/app.js
+++ feeler_back/app.js
@@ -2,6 +2,12 @@
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 
 var createError = require('http-errors');
 var express = require('express');
