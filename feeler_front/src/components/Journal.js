d3dc5dcdee6ec87a6bf72e1a880b8d51cd4f1ec2
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-11-20 06:16:45 -0500

    T update Journal.js

--- feeler_front/src/components/Journal.js
+++ feeler_front/src/components/Journal.js
@@ -43,7 +43,7 @@ export default class Journal extends React.Component {
             <>
             <h1> Journal</h1>
 
-            <div style={{width: "100%", height: '100%'}}>
+            <div style={{ display: 'flex', height: '100%' }}>
                 <DataGrid
                     rows={rows}
                     columns={columns}
