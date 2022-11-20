b861b1bcbb45deb481dbf80f054763b71dffbb29
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-11-20 15:38:16 -0500

    T update app.js

--- feeler_back/app.js
+++ feeler_back/app.js
@@ -21,45 +21,45 @@ import indexRouter from './routes/index.js';
 import UsersRouter from "./routes/users.js";
 import Get_UsernameRouter from "./routes/get_username.js";
 
+export default function App() {
 
-var app = express();
-app.use(cors())
+  var app = express();
+  app.use(cors())
 
-app.set('port', process.env.PORT || 3001)
+  app.set('port', process.env.PORT || 3001)
 
-app.listen(app.get('port'), () => {
-  console.log(`Express server listening on port ${app.get('port')}`);
-});
+  app.listen(app.get('port'), () => {
+    console.log(`Express server listening on port ${app.get('port')}`);
+  });
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+  // view engine setup
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'jade');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+  app.use(logger('dev'));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, 'public')));
 
-// Use Router
-app.use('/', indexRouter);
-app.use('/users', UsersRouter);
-app.use("/get_username", Get_UsernameRouter);
+  // Use Router
+  app.use('/', indexRouter);
+  app.use('/users', UsersRouter);
+  app.use("/get_username", Get_UsernameRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+  // catch 404 and forward to error handler
+  app.use(function(req, res, next) {
+    next(createError(404));
+  });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // error handler
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+}
