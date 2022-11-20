5fe3fc1a50d1cd2f6361482b18928699320d98f6
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-11-20 05:36:06 -0500

    T update stats.js

--- feeler_front/src/components/stats.js
+++ feeler_front/src/components/stats.js
@@ -161,17 +161,17 @@ export default class Stats extends React.Component {
 
             <div style={{width: "100%"}}>
                 
-                <div style={{display: "inline-block", width: "100%"}}>
+                <div style={{display: "inline-block"}}>
                 <h2> CPU & RAM % </h2>
                     <Line data= {this.state.tables.table1}> </Line>
                 </div>
                 
-                <div style={{display: "inline-block", width: "100%"}}>
+                <div style={{display: "inline-block"}}>
                 <h2> Network </h2>
                 <Line data= {this.state.tables.table2}> </Line>
                 </div>
                 
-                <div style={{display: "inline-block", width: "100%"}}>
+                <div style={{display: "inline-block"}}>
                 <h2> Network Speed </h2>
                 <Line data= {this.state.tables.table3}> </Line>
                 </div>
