efb1a8d16240f6c2346c65a8605ad943a1e319d9
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-11-20 15:36:38 -0500

    T update app.js

--- feeler_back/app.js
+++ feeler_back/app.js
@@ -6,7 +6,7 @@ const require = createRequire(import.meta.url);
 import path from 'path';
 import {fileURLToPath} from 'url';
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.__dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 
 
