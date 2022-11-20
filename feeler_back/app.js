9913e95c65a8ceec02b0506ffb04d6954b8ea88f
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-11-20 15:33:33 -0500

    T update app.js

--- feeler_back/app.js
+++ feeler_back/app.js
@@ -12,7 +12,6 @@ const __dirname = __dirname(__filename);
 
 var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors')
