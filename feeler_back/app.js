1a95fc78501a0b1b7f06ca456d6ccd1c7a620774
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-11-21 10:20:46 -0500

    T update app.js

--- feeler_back/app.js
+++ feeler_back/app.js
@@ -43,7 +43,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Use Router
 
-app.use('/', indexRouter);
+app.use(indexRouter);
 /*
 app.use('/users', usersRouter);
 app.use("/get_username", getUsernameRouter);
