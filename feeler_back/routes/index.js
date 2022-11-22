a4b29055f144be53cc761ecffdca115459debbac
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-11-21 21:27:11 -0500

    T update app.js and feeler_back/routes/index.js

--- feeler_back/routes/index.js
+++ feeler_back/routes/index.js
@@ -7,10 +7,6 @@ export default function indexRouter() {
 
   var express = require('express');
   var router = express.Router();
-
-  router.use(function (req, res) {
-    console.log("index test");
-  })
   
   /* GET home page. */
   router.get('/', (req, res) => {
