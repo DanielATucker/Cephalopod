ab10e71da8e4300268f1896f70533f446b884bea
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-11-21 22:09:40 -0500

    T update Database.js

--- feeler_back/components/Database.js
+++ feeler_back/components/Database.js
@@ -2,55 +2,48 @@
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 
-const neo4j = require('neo4j-driver')
 
 export default async function Database(query) {
-    const Neo4jUser = process.env.Neo4jUser
-    const Neo4jPass = process.env.Neo4jPass
+  const Neo4jUser = process.env.Neo4jUser;
+  const Neo4jPass = process.env.Neo4jPass;
 
-    const uri = "bolt://100.69.19.3/7688"
+  const uri = "100.69.19.3/7688";
 
-    try {
-      const driver = neo4j.driver(uri, neo4j.auth.basic(Neo4jUser, Neo4jPass))
-      const session = driver.session()
-    }
-    catch (err) {
+  try {
+    const driver = neo4j.driver(uri, neo4j.auth.basic(Neo4jUser, Neo4jPass));
+    const session = driver.session();
+  }
+  catch (err) {
+    console.log(err);
+  }
+  /*
+  const result = null;
+
+  const node = null;
+
+  try {
+    result = await session.run(query);
+
+    console.log(result);
+
+    const singleRecord = result.records[0];
+    
+    console.log(singleRecord);
+    
+    node = singleRecord.get(0);
+
+    console.log(node);
+  } finally {
+    await session.close()
+  }
+
+  try {
+      return node
+  }
+  catch (err) {
       console.log(err);
-
-    }
-      
-    
-    const result = null;
-
-    const node = null;
-
-    try {
-      result = await session.run(query);
-
-      console.log(result);
-
-      const singleRecord = result.records[0];
-      
-      console.log(singleRecord);
-      
-      node = singleRecord.get(0);
-
-      console.log(node);
-    } finally {
-      try {
-        await session.close()
-      }
-      catch (err) {
-        console.log(err);
-      }
-    }
-
-    try {
-        return node
-    }
-    catch (err) {
-        console.log(err);
-    }
-    
-    await driver.close()
+  }
+  
+  await driver.close()
+  */
 };
