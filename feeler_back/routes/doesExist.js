cef9de670126a35ebfc9fb1d900a5d4a10e1b221
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-11-23 11:30:00 -0500

    T update doesExist.js

--- feeler_back/routes/doesExist.js
+++ feeler_back/routes/doesExist.js
@@ -5,7 +5,7 @@ const require = createRequire(import.meta.url);
 var express = require('express');
 var router = express.Router();
 
-import Database from "../components/Database";
+import Database from "../components/Database.js";
 
 router.get('/', (req, res) => {
   let nodePromise = Database("MATCH (n: Main) WHERE n.name='Main' RETURN (n) ");
