9ba5960d6ab6b6dc733de2d2f093bc44c767a8a0
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-11-26 11:20:47 -0500

    T update graphInit.js

--- feeler_front/src/components/modules/graphInit.js
+++ feeler_front/src/components/modules/graphInit.js
@@ -37,6 +37,8 @@ export default class GraphInit extends React.Component {
 
     componentDidUpdate() {
         setTimeout(this.doesExist, 10000);
+
+        setTimeout(this.isDatbaseOnline, 10000);
     }
 
     render() {
