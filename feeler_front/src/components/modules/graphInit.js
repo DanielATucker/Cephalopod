aaff2a017dfea0fddc9cdba3d1dde7e1968a87d5
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-11-27 14:36:15 -0500

    T update graphInit.js

--- feeler_front/src/components/modules/graphInit.js
+++ feeler_front/src/components/modules/graphInit.js
@@ -91,7 +91,7 @@ export default class GraphInit extends React.Component {
     handleUsernameSubmit(event) {
         console.log(event)
  
-        console.log(this.state.usernameInput);
+        console.log(JSON.stringify(this.state.usernameInput));
         
         event.preventDefault();
     };
