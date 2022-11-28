2cd785882508ac04be2e7e472d090b288f876b78
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-11-27 19:47:15 -0500

    T update system.js

--- feeler_back/routes/system.js
+++ feeler_back/routes/system.js
@@ -40,7 +40,7 @@ router.get('/systeminit', (req, res) => {
   }));
 });
 
-router.post('/newUser', (req, res) => {
+router.get('/newUser', (req, res) => {
   console.log("TEST");
   
   let session = req.session;
