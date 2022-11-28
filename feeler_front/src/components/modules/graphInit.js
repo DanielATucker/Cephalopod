e2dc48d1ee93f2fd91aa17c7ab62d83d37a27e55
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-11-28 02:11:03 -0500

    T update graphInit.js

--- feeler_front/src/components/modules/graphInit.js
+++ feeler_front/src/components/modules/graphInit.js
@@ -168,7 +168,7 @@ export default class GraphInit extends React.Component {
     componentDidUpdate(prevState) {
         if (this.state !== prevState) {
             
-            if ((this.state.test != prevState.test) && (this.state.doesExist === "System found.") && (this.state.username === "Pending")) {
+            if ((this.state.doesExist != prevState.doesExist) && (this.state.doesExist === "System found.") && (this.state.username === "Pending")) {
                 this.setState({
                     "usernameInitFormStatus": true
                 });
