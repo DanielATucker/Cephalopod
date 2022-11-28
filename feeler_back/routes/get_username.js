2a994f6d6b70dc6b9a9260268e56105580846ee1
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-11-27 20:54:30 -0500

    T update get_username.js

--- feeler_back/routes/get_username.js
+++ feeler_back/routes/get_username.js
@@ -11,7 +11,35 @@ var express = require('express');
 
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
+
+router.post('/', function (req, res) {    
+  let username = req.body.username;
+
+  let password = req.body.password;
+
+  let nodePromise = Database(`MATCH (n: User) WHERE n.name = '${username}' AND n.password = '${password}' RETURN (n.name)`);
+
+  nodePromise.then((response) => {
+    if ((typeof response !== 'undefined') && ( response != null)) {
+      if ((typeof response == "string") && (response == "No Database found")) {
+        res.json({
+          "USERNAME":"No Database found"
+        });
+      }
+      else {
+        console.log(`Response ${JSON.stringify(response)}`);
+        console.log(`Username Submitted ${username}`)
+
+        res.json({"USERNAME": response});
+      };
+    }
+    else {
+      res.json({"USERNAME": "Username not Found in database"});
+    }
+  });
+});
+
+router.post('/', function(req, res, next) {
   let nodePromise = Database("MATCH (n)  WHERE n.name = 'test' RETURN (n)");
 
   nodePromise.then((response) => {
