5fd3bf5fc69666dae568f165b20424080feb5ed8
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-11-27 19:44:21 -0500

    T update system.js

--- feeler_back/routes/system.js
+++ feeler_back/routes/system.js
@@ -56,6 +56,8 @@ router.post('/newUser', (req, res) => {
   Database("MATCH (m: Main) CREATE (u: User), (T: TaskMaster), (J: JournalMaster), (TC: TaskCompleted), (u)-[r: link]->(m), (J)-[s: link]->(u), (T)-[t: link]->(u), (TC)-[l: link]->(T) SET u.name = '{username}', T.name = 'TaskMaster', J.name = 'JournalMaster', TC.name = 'TaskCompleted' ", username=username);
 
   Database("MATCH (u: User) WHERE u.name = '{user}' SET u.user = '{user}', u.password = '{password}', u.privileges = 'user' ", user=username, password=password);
+
+  res.json({message: 'message here'});
 });
 
 export default router;
