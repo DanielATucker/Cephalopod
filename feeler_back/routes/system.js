65c2614bfb9485d4c64722b078cfccb0ee564233
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-11-28 11:41:49 -0500

    T update system.js

--- feeler_back/routes/system.js
+++ feeler_back/routes/system.js
@@ -74,6 +74,9 @@ router.post('/newUser', function (req, res) {
   console.log(username);
   console.log(password);
 
+  let session = req.session;
+  console.log(`Session: ${session}`);
+
   let now = strftime("%y%m%d_%X");
 
   Database(`MATCH (m: Main) CREATE (u: User), (T: TaskMaster), (J: JournalMaster), (TC: TaskCompleted), (u)-[r: link]->(m), (J)-[s: link]->(u), (T)-[t: link]->(u), (TC)-[l: link]->(T) SET u.name = '${username}', u.password = '${password}', u.privileges = 'user', u.loginHistory = '${JSON.stringify([{"createdTime": now}])}', u.createdTime = '${now}', T.name = 'TaskMaster', J.name = 'JournalMaster', TC.name = 'TaskCompleted'`);
@@ -99,7 +102,7 @@ router.post('/login', function (req, res) {
     let now = {};
 
     now[nowRaw] = "Logged in";
-    
+
     let loginHistory = node.properties.loginHistory;
     console.log(JSON.stringify(loginHistory));
 
