d8c3a0a1d5ff96ee44a05a86a675dce837a4122b
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-11-28 08:22:04 -0500

    T update system.js

--- feeler_back/routes/system.js
+++ feeler_back/routes/system.js
@@ -72,10 +72,7 @@ router.post('/newUser', function (req, res) {
   console.log(username);
   console.log(password);
 
-  Database(`MATCH (m: Main) CREATE (u: User), (T: TaskMaster), (J: JournalMaster), (TC: TaskCompleted), (u)-[r: link]->(m), (J)-[s: link]->(u), (T)-[t: link]->(u), (TC)-[l: link]->(T) SET u.name = '${username}', T.name = 'TaskMaster', J.name = 'JournalMaster', TC.name = 'TaskCompleted'`);
-
-  Database(`MATCH (u: User) WHERE u.name = '${username}' SET u.password = '${password}', u.privileges = 'user' `);
-
+  Database(`MATCH (m: Main) CREATE (u: User), (T: TaskMaster), (J: JournalMaster), (TC: TaskCompleted), (u)-[r: link]->(m), (J)-[s: link]->(u), (T)-[t: link]->(u), (TC)-[l: link]->(T) SET u.name = '${username}', u.password = '${password}', u.privileges = 'user', T.name = 'TaskMaster', J.name = 'JournalMaster', TC.name = 'TaskCompleted'`);
   res.json(JSON.stringify({message: 'message here'}));
 });
 
