46f9a3fb1b1e29302980ad843c45980ff0049e97
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-11-29 07:41:47 -0500

    T update system.js

--- feeler_back/routes/system.js
+++ feeler_back/routes/system.js
@@ -120,6 +120,7 @@ router.get('/getUsername', (req, res) => {
   nodePromise.then(node => {
     if ((typeof node !== 'undefined') && ( node !== null) && (node !== "No Database found")) {
       console.log(`NODE ${JSON.stringify(node)}`);
+      console.log(`User Id: ${userId}`);
     }
     else {
       res.json({"username": "Not logged in"});
