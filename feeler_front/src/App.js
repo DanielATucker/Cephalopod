37f6d5d154a915d3bacf14782059628ed4e83ce1
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-11-29 06:41:10 -0500

    Added theme update 1 file and delete 1 file

--- feeler_front/src/App.js
+++ feeler_front/src/App.js
@@ -2,14 +2,19 @@ import React, { Component } from "react";
 import { Card } from '@mui/material';
 
 import Main from "./components/main.js";
+import { Theme } from "./theme.js";
 
 
 export default class App extends Component {
   render() { 
     return (
+      <>
+      <ThemeProvider theme={Theme}>
       <Card variant="outlined">
-        <Main/>
+        <Main />
       </Card>
+      </ThemeProvider>
+      </>
     )
   };
 }
---
+++ feeler_front/src/theme.js
@@ -0,0 +1,20 @@
+import { ThemeOptions } from '@material-ui/core/styles/createMuiTheme';
+import { createTheme } from '@mui/material/styles';
+
+export const Theme = createTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: '#d20104',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+    text: {
+      primary: '#d20104',
+    },
+    background: {
+      default: '#120f0f',
+    },
+  },
+});
