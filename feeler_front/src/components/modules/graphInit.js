0f137837dae3a04934a9f294d77083664826830c
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-11-28 23:05:39 -0500

    T update graphInit.js

--- feeler_front/src/components/modules/graphInit.js
+++ feeler_front/src/components/modules/graphInit.js
@@ -36,7 +36,7 @@ export default class GraphInit extends React.Component {
         
         let node = await userResponse.json();
         
-        if ((node !== "undefined") && (node !== "null")) {
+        if ((node !== "undefined") && (node !== "null") && (node.username !== "Not logged in")) {
             console.log(`NODE: ${JSON.stringify(node)}`);
             console.log(`USERNAME: ${JSON.stringify(node.username)}`);
 
