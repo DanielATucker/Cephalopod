4c0133ff2ce8a57cc103e1220526d63b828a0b6d
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-12-02 11:54:46 -0500

    T update editor.js

--- feeler_front/src/components/journal/editor.js
+++ feeler_front/src/components/journal/editor.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { React, useEffect } from 'react';
 
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
