06747e555fc2d1536a9cfb5ce6573ce2ac0c8b7b
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-12-02 00:17:16 -0500

    T update journal.js

--- feeler_front/src/components/journal.js
+++ feeler_front/src/components/journal.js
@@ -62,7 +62,7 @@ export default class Journal extends React.Component {
                 </div>
 
                 <div style={{display: "inline-block"}}>
-                    <Card variant="outlined" border="1px" borderColor="#d20104"> Viewer </Card>
+                    <Card variant="outlined" border={3} borderColor="#d20104"> Viewer </Card>
                 </div>
             </div>
 
