cd6f7467afa07f3ba29dad3c49f6ca46aaf82fd2
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-12-02 09:35:08 -0500

    T update editor.js

--- feeler_front/src/components/journal/editor.js
+++ feeler_front/src/components/journal/editor.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 
 export default class Editor extends React.Component {
