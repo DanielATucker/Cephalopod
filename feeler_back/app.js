cc5fa6eb9c68cee0a6c10d34372fbc365b1f13ce
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-12-01 22:36:55 -0500

    T update app.js

--- feeler_back/app.js
+++ feeler_back/app.js
@@ -63,23 +63,6 @@ app.set('trust proxy', 1) // trust first proxy
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// set a cookie
-app.use(function (req, res, next) {
-  // check if client sent cookie
-  var cookie = req.cookies.cookieName;
-  if (cookie === undefined) {
-    // no: set a new cookie
-    var randomNumber=Math.random().toString();
-    randomNumber=randomNumber.substring(2,randomNumber.length);
-    res.cookie('cookieName',randomNumber, { maxAge: 900000, httpOnly: true });
-    console.log('cookie created successfully');
-  } else {
-    // yes, cookie was already present 
-    console.log('cookie exists', cookie);
-  } 
-  next(); // <-- important!
-});
-
 //Init session
 app.use(session({ 
   secret: 'keyboard cat',
