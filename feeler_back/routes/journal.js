65d988dde3658835a7e77bdebcc8aa53770e6880
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-12-04 11:42:32 -0500

    T update journal.js

--- feeler_back/routes/journal.js
+++ feeler_back/routes/journal.js
@@ -9,7 +9,7 @@ var router = express.Router();
 
 router.post('/', function(req, res, next) {    
     data = req.body.journalData
-    Database(`MATCH (U: User {name: ${req.session.username}}), (JM: JournalMaster) WHERE (JM)-[la: link]->(U) MERGE (J: Journal {body: ${data}}), (J)-[lb: Journal]-(JM)`);
+    Database(`MATCH (JM: JournalMaster)-[la: link]->(U: User {name: '${req.session.username}'}) MERGE (J: Journal {body: '${data}'})-[Jo: JournalOf]->(JM) ON MATCH  SET J.body = '${data}'`);
 
     res.send();
     
