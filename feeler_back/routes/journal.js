138f14aca4168dbb9343ecf59efa0d45ff10460b
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-12-04 13:27:13 -0500

    T update journal.js and system.js

--- feeler_back/routes/journal.js
+++ feeler_back/routes/journal.js
@@ -12,27 +12,7 @@ router.post('/', function(req, res) {
     let data = req.body.journalData;
     let username = req.session.username;
 
-    console.log(req.session)
-    console.log(username);
-    console.log(data);
-
-    let nodePromise = Database(`MATCH (JM: JournalMaster)-[la: link]->(U: User {name: '${username}'}) MERGE (J: Journal {body: '${data}'})-[Jo: JournalOf]->(JM) ON MATCH SET J.body = '${data}' RETURN (J)`);
-
-    nodePromise.then((node) => {
-        if ((typeof node !== 'undefined') && ( node != null)) {
-          if (node == "No Database found") {
-            res.json({
-              "doesExist" : "No Database found. Recommended, Start database"
-            })
-          }
-          else {
-            console.log(`Node: ${JSON.stringify(node)}`);
-          }
-        }
-        else {
-            console.log("No node found");
-        }
-    });
+    Database(`MATCH (JM: JournalMaster)-[la: link]->(U: User {name: '${username}'}) MERGE (J: Journal)-[Jo: JournalOf]->(JM) ON CREATE J.body = '${data}' ON MATCH SET J.body = '${data}'`);
 
     res.send();
 });
--- feeler_back/routes/system.js
+++ feeler_back/routes/system.js
@@ -62,7 +62,7 @@ router.get('/doesUserExist', (req, res) => {
 });
 
 router.get('/systeminit', (req, res) => {
-  Database("CREATE (m: Main), (s: System), (S: SessionMaster), (S)-[a: link]->(s)-[b: link]->(m) SET m.name = 'Main', s.name = 'System', S.name = 'SessionMaster'");
+  Database("CREATE (m: Main) SET m.name = 'Main'");
 
   res.json(JSON.stringify({
     "status": "success",
