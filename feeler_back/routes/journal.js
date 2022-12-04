06453cb4920eae9f0a96a8934ac8310facc87503
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-12-04 15:16:37 -0500

    Added JournalTitle update journal.js

--- feeler_back/routes/journal.js
+++ feeler_back/routes/journal.js
@@ -12,11 +12,14 @@ var router = express.Router();
 
 router.post('/', function(req, res) {    
     let data = req.body.journalData;
+    
     let username = req.session.username;
 
     let now = strftime('%y%m%d_%X');
-    
-    Database(`MATCH (JM: JournalMaster)-[la: link]->(U: User {name: '${username}'}) MERGE (J: Journal)-[Jo: JournalOf]->(JM) ON CREATE SET J.body = '${data}', set J.createdOn = '${now}' ON MATCH SET J.body = '${data}', J.lastEdit = '${now}'`);
+
+    let journalTitle = (`Journal %{strftime('%y%m%d')}`);
+
+    Database(`MATCH (JM: JournalMaster)-[la: link]->(U: User {name: '${username}'}) MERGE (J: Journal)-[Jo: JournalOf]->(JM) ON CREATE SET J.name = '${journalTitle}', J.body = '${data}', J.createdOn = '${now}' ON MATCH SET J.body = '${data}', J.lastEdit = '${now}'`);
 
     res.send();
 });
