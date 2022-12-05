cfe2b542d02cefbd1bfce188d6ba4ec508b1193e
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-12-05 00:02:45 -0500

    T update journal.js

--- feeler_back/routes/journal.js
+++ feeler_back/routes/journal.js
@@ -35,9 +35,9 @@ router.get('/get_journal', function(req, res) {
             });
           }
           else {
-            console.log(result);
+            console.log(`Result: ${result.properties}`);
             
-            res.json(JSON.stringify(result));
+            res.json(JSON.stringify(result.properties));
           };
         }
         else {
