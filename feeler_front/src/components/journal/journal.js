6c7356d98b4bf0ccdcf56dfb3aedac9683b2a54b
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-12-04 21:01:34 -0500

    T update journal.js

--- feeler_front/src/components/journal/journal.js
+++ feeler_front/src/components/journal/journal.js
@@ -25,7 +25,7 @@ export  default class Journal extends React.Component {
         const response = await fetch('https://100.69.19.3:3001/journal/get_journal', {
             method: 'GET',
             credentials: "include"
-        }).next((journalTitles) => {
+        }).then((journalTitles) => {
             console.log(JSON.stringify(journalTitles));
 
             this.setState({
