402c42025e8b05d6443fac090dd4eafeb1598ec9
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-12-04 21:50:50 -0500

    T update journal.js

--- feeler_back/routes/journal.js
+++ feeler_back/routes/journal.js
@@ -25,7 +25,7 @@ router.post('/post_journal', function(req, res) {
 });
 
 router.get('/get_journal', function(req, res) {
-    let nodePromise = Database(`MATCH (J: Journal)-[la: ]->(JM: JournalMaster)-[lb]->(U: User {name: '${req.session.username}'}) RETURN (J))`);
+    let nodePromise = Database(`MATCH (J: Journal)-[la]->(JM: JournalMaster)-[lb]->(U: User {name: '${req.session.username}'}) RETURN (J))`);
 
     nodePromise.then((node) => {
         if ((typeof node !== 'undefined') && ( node != null)) {
