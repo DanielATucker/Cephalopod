96d1c0a773e47a74d4dc25c634d52d58cb5744cb
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-12-04 22:40:44 -0500

    T update journal.js and graphInit.js

--- feeler_front/src/components/journal/journal.js
+++ feeler_front/src/components/journal/journal.js
@@ -25,10 +25,10 @@ export  default class Journal extends React.Component {
         const response = await fetch('https://100.69.19.3:3001/journal/get_journal', {
             method: 'GET',
             credentials: "include"
-        });
-
-        response.next((node) => {
+        }).then((node) => {
             console.log(node);
+        }).catch((err) => {
+            console.log(err);
         });
     };
 
