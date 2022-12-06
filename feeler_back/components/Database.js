77d2f286ab392663f78638ea2b02d7208c889e16
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-12-06 12:07:07 -0500

    T update Database.js

--- feeler_back/components/Database.js
+++ feeler_back/components/Database.js
@@ -35,7 +35,7 @@ export default async function Database(query) {
     catch (err) {
       console.log(`Error ${err}`);
 
-      console.log(`Second Record: ${record.values()}`);
+      console.log(`Second Record: ${JSON.stringify(record.values())}`);
     }
 
     await session.close()
