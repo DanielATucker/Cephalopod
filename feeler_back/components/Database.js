f5e8a0ce66a317a50c301500a64f08fa776be109
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-12-06 13:00:58 -0500

    T update Database.js

--- feeler_back/components/Database.js
+++ feeler_back/components/Database.js
@@ -38,7 +38,7 @@ export default async function Database(query) {
 
         fields = fieldsList[0];
 
-        console.log(JSON.stringify(fields.properties));
+        console.log(fields);
       }
       catch (err) {
         console.log(`Request Conformation`);
