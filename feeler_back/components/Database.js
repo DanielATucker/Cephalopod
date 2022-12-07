7583e6bb13e7bd651cd44b2e5b35aa8feb30e396
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-12-06 20:46:40 -0500

    T update Database.js

--- feeler_back/components/Database.js
+++ feeler_back/components/Database.js
@@ -33,7 +33,7 @@ export default async function Database(query) {
       console.log(JSON.stringify(nodeList));
     };
 
-    records.map(nodeListCall(), async record => {
+    records.map(async (record) => {
       record = Object.values(records)[0];
     
       console.log(`Typeof: ${record}`);
