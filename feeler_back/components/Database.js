40eff886a7bd38231a5201fbad00e7aefc07bfc5
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-12-06 22:43:39 -0500

    T update Database.js

--- feeler_back/components/Database.js
+++ feeler_back/components/Database.js
@@ -37,7 +37,7 @@ export default async function Database(query) {
     
     console.log(JSON.stringify(nodeList));
 
-    console.log(result);
+    console.log(JSON.stringify(result), null, 2);
 
     if (Object.keys(nodeList).length === 1){
       console.log(nodeList[0]);
