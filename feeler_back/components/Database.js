ca8b897e3ac39537d19cd7c751c561bafdd997d4
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-12-07 15:50:01 -0500

    T update Database.js

--- feeler_back/components/Database.js
+++ feeler_back/components/Database.js
@@ -33,7 +33,9 @@ export default async function Database(query) {
       return properties;
     };
 
-    let nodeList = records.map(get_record);
+    let nodeList = []
+    
+    nodeList.concat(records.map(get_record));
     
     console.log(JSON.stringify(nodeList));
 
