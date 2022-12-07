d977bdb2b55a67326c390c346245dc896bcdf804
DanielATucker <70778655+DanielATucker@users.noreply.github.com>
2022-12-07 16:10:43 -0500

    T update Database.js

--- feeler_back/components/Database.js
+++ feeler_back/components/Database.js
@@ -21,14 +21,8 @@ export default async function Database(query) {
     
     let records = Object.values(result)[0];
     
-    if (records.values.length === 1) {
-      console.log(1);
-    };
-
-    if (records.values.length === 2) {
-      console.log(2);
-    };
-
+    console.log(records.keys.length);
+    
     function get_record(record) {
       record = Object.values(records)[0];
         
