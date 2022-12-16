a783bf83ca1d2f5a5f71b703847e8030ad9cfe8d
Daniel Tucker <DanielATucker@Outlook.com>
2022-12-16 16:03:39 -0500

    Working 22216 Removed logs Datagrid showing Journals

--- feeler_front/src/components/journal/journal.js
+++ feeler_front/src/components/journal/journal.js
@@ -24,14 +24,11 @@ export  default class Journal extends React.Component {
         };
     };
 
-
-
     componentDidMount() {
         this.getJournalData();
 
         setInterval(this.getJournalData, 10000);
     };
-
 
     getJournalData = async () => {
         const response = await fetch('https://100.69.19.3:3001/journal/get_journal', {
@@ -53,52 +50,28 @@ export  default class Journal extends React.Component {
         console.log(`Node: ${JSON.stringify(node)}`);
 
         let journals = JSON.parse(node);
-        
-        console.log(typeof journals);
-        console.log(`Journals: ${JSON.stringify(journals)}`);
-            
+                    
         let prevGrid = JSON.parse(JSON.stringify(this.state.datagrid));
 
-        console.log(prevGrid);
-        console.log(prevGrid.rows);
-
         Object.entries(journals).forEach((journalObject, count) => {           
-            console.log(`JournalObject: ${JSON.stringify(journalObject)}`);
-
             let journal = journalObject[1];
-
-            console.log(`Journal: ${JSON.stringify(journal)}`);
-
-            console.log(`PrevGrid Rows in: ${JSON.stringify(prevGrid.rows)}`);
 
             if (prevGrid.rows.some(found => found.name === journal.name)) {
                 console.log(`FOUND JOURNAL NAME: ${journal.name}`);
             }
             else{
-                console.log(`Count in: ${count}`);
-
                 count = count + 1;
 
-                console.log(`Count out: ${count}`);
-
                 prevGrid.rows.push({ id: count, name: journal.name});
-
-                console.log(`PrevGrid out: ${JSON.stringify(prevGrid)}`);
 
                 count++;
             };
         });
 
-        console.log(`Grid out: ${JSON.stringify(prevGrid)}`);
-
         if (this.state) {
             this.setState({
                 "datagrid": prevGrid
             });
-        };
-
-        if (this.state) {
-            console.log(`Updated DataGrid: ${JSON.stringify(this.state.datagrid)}`);
         };
     };
 
