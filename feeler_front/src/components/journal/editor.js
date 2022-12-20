59d68c72d91af169edcfa3b183c74c2197db3b50
Daniel Tucker <DanielATucker@Outlook.com>
2022-12-19 21:32:36 -0500

    Revert Bug Noticed (BN) Can't update or view current day T

--- feeler_front/src/components/journal/editor.js
+++ feeler_front/src/components/journal/editor.js
@@ -43,7 +43,7 @@ export default class Editor extends React.Component {
 
         <CKEditor
         editor={ ClassicEditor }
-        data={this.state.data}
+        data={this.state.journalData}
         onChange={ ( event, editor ) => {
           const data = editor.getData();
           this.setState({
