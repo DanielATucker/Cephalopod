32847dbb059128c2d92f223916808c41599a1ce7
Daniel Tucker <DanielATucker@Outlook.com>
2023-01-13 18:34:03 -0500

    T

--- feeler_back/routes/journal.js
+++ feeler_back/routes/journal.js
@@ -10,18 +10,18 @@ var express = require('express');
 
 var router = express.Router();
 
-router.post('/post_journal', function(req, res) {    
-    let data = req.body.journalData;
+router.post('/post_journal', function(req, res) {
+  let data = req.body.journalData;
     
-    let username = req.session.username;
+  let username = req.session.username;
 
-    let now = strftime('%y%m%d_%X');
+  let now = strftime('%y%m%d_%X');
 
-    let journalTitle = (`Journal ${strftime('%y%m%d')}`);
+  let journalTitle = (`Journal ${strftime('%y%m%d')}`);
 
-    Database(`MATCH (JM: JournalMaster)-[la: link]->(U: User {name: '${username}'}) MERGE (J: Journal {name: '${journalTitle}'})-[Jo: JournalOf]->(JM) ON CREATE SET J.name = '${journalTitle}', J.body = '${data}', J.createdOn = '${now}' ON MATCH SET J.body = '${data}', J.lastEdit = '${now}'`);
+  Database(`MATCH (JM: JournalMaster)-[la: link]->(U: User {name: '${username}'}) MERGE (J: Journal {name: '${journalTitle}'})-[Jo: JournalOf]->(JM) ON CREATE SET J.name = '${journalTitle}', J.body = '${data}', J.createdOn = '${now}' ON MATCH SET J.body = '${data}', J.lastEdit = '${now}'`);
 
-    res.send();
+  res.send();
 });
 
 router.get('/get_journal', function(req, res) {
