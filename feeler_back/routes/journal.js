b072e2b591c88e85a0816e5aa2e21642109d66a4
Daniel Tucker <DanielATucker@Outlook.com>
2023-02-07 08:49:07 -0500

    T

--- feeler_back/routes/journal.js
+++ feeler_back/routes/journal.js
@@ -13,7 +13,7 @@ var router = express.Router();
 router.post('/post_journal/:journalTitle', function(req, res) {    
   try{
 
-    console.log(`REQ Params ${JSON.stringify(req.params)}`);
+    console.log(`REQ ${JSON.stringify(req)}`);
 
     let journalTitle = req.params.journalTitle;
   
