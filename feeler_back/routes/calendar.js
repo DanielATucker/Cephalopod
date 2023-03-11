5ecffe23e8e387fe6f834e826a7b3a7410e600b5
Daniel Tucker <DanielATucker@Outlook.com>
2023-03-11 17:50:22 -0500

    Removed calendar Re-added new calendar

--- feeler_back/routes/calendar.js
+++ feeler_back/routes/calendar.js
@@ -9,9 +9,30 @@ var express = require('express');
 var router = express.Router();
   
 /* GET home page. */
-router.get('/add_event', (req, res) => {
-  res.send('working')
-});
+router.post('/add_event/:eventTitle', function(req, res) {    
+    try{
+      console.log(`DATA ${req.body}`)
+  
+      let eventTitle = req.body.eventName;
+    
+      console.log(`Title: ${eventTitle}`);
+  
+      let data = req.body.eventData;
+  
+      console.log(`Data: ${JSON.stringify(data)}`);
+  
+      let username = req.session.username;
+  
+      let now = strftime('%y%m%d_%X');
+      
+      //Database(`MATCH (CM: CalendarMaster)-[la]->(U: User {name: '${username}'}) MERGE (J: Journal {name: '${journalTitle}'})-[Jo: JournalOf]->(JM) ON CREATE SET J.name = '${journalTitle}', J.body = '${data}', J.createdOn = '${now}' ON MATCH SET J.body = '${data}', J.lastEdit = '${now}'`);
+  
+      res.json("working");
+    }
+    catch (err) {
+      console.log(`ERROR: ${err}`);
+    };
+  });
 
 router.get('/get_events', (req, res) => {
     let nodePromise = Database(`MATCH (c: *)-[la]->(CM: CalendarMaster)-[lb]->(U: User {name: '${req.session.username}'}) RETURN (c)`);
