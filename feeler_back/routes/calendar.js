f11656cb747a7b8cf6d04ebd0b11a45bd5753249
Daniel Tucker <DanielATucker@Outlook.com>
2023-03-11 11:24:56 -0500

    Added (A) database lookup to feeler_back calendar

--- feeler_back/routes/calendar.js
+++ feeler_back/routes/calendar.js
@@ -2,6 +2,8 @@
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 
+import Database from "../components/Database.js";
+
 
 var express = require('express');
 var router = express.Router();
@@ -12,6 +14,27 @@ router.get('/add_event', (req, res) => {
 });
 
 router.get('/get_events', (req, res) => {
+    let nodePromise = Database(`MATCH (c: *)-[la]->(CM: CalendarMaster)-[lb]->(U: User {name: '${req.session.username}'}) RETURN (c)`);
+
+    nodePromise.then((result) => {
+        if ((typeof result !== 'undefined') && ( result != null)) {
+          if (result == "No Database found") {
+            res.json({
+              "doesExist" : "No Database found. Recommended, Start database"
+            });
+          }
+          else {
+            console.log(`Calendar: ${result}`);
+
+            //res.json(JSON.stringify(result));
+          };
+        }
+        else {
+            console.log("No node found");
+
+            res.json(`No node found`)
+        };
+    });
     res.json('working')
   });
 
