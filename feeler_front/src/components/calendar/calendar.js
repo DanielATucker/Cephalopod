9a02c0308249bf7630bcea67646b0e48830f789a
Daniel Tucker <DanielATucker@Outlook.com>
2023-03-11 09:46:38 -0500

    Added Calendar

---
+++ feeler_front/src/components/calendar/calendar.js
@@ -0,0 +1,59 @@
+import React, from 'react';
+import {DayPilot, DayPilotCalendar, DayPilotNavigator} from "@daypilot/daypilot-lite-react";
+import "./CalendarStyles.css";
+
+const styles = {
+  wrap: {
+    display: "flex"
+  },
+  left: {
+    marginRight: "10px"
+  },
+  main: {
+    flexGrow: "1"
+  }
+};
+
+class Calendar extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.calendarRef = React.createRef();
+    this.state = {
+      viewType: "Week",
+      durationBarVisible: false
+    };
+  }
+
+  get calendar() {
+    return this.calendarRef.current.control;
+  }
+
+  render() {
+    const {...config} = this.state;
+    return (
+      <div style={styles.wrap}>
+        <div style={styles.left}>
+          <DayPilotNavigator
+            selectMode={"week"}
+            showMonths={3}
+            skipMonths={3}
+            onTimeRangeSelected={ args => {
+              this.calendar.update({
+                startDate: args.day
+              });
+            }}
+          />
+        </div>
+        <div style={styles.main}>
+        <DayPilotCalendar
+          {...config}
+          ref={this.calendarRef}
+        />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Calendar;
