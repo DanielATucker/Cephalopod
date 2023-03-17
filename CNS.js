4c433c944f5ab539dfb002a96b4d7a822672724a
Daniel Tucker <DanielATucker@Outlook.com>
2023-03-16 21:02:57 -0400

    T

--- CNS.js
+++ CNS.js
@@ -8,6 +8,8 @@ import GUN from "gun";
 var WebSocketServer = require('ws').Server
 , wss = new WebSocketServer({ port: 3006 });
 
+console.log(`CNS Online`)
+
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
     console.log('received:', message);
