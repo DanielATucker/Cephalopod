e5e9581dee2e2053837145abe73e862b4fbe6f70
Daniel Tucker <DanielATucker@Outlook.com>
2023-03-17 12:04:56 -0400

    T

--- CNS.js
+++ CNS.js
@@ -7,16 +7,13 @@ const Gun = require('gun');
 
 
 export default function CNS() {
-  var startGunServer = require('gun-server');
-  var gun = startGunServer();
-
   var server = require('gun-server');
 
   server({
     handler: function (request, response) {
       // request handler (optional)
     },
-    port: 8080, // default, optional
+    port: 3006, // default, optional
     server: new http.Server(), // optional
     options: {
       // this is passed to the gun constructor
