90fe5927fff8f51526dbfd823121ec1d9c01c3d3
Daniel Tucker <DanielATucker@Outlook.com>
2023-03-16 22:29:44 -0400

    T

--- CNS.js
+++ CNS.js
@@ -2,13 +2,30 @@
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 
+var fs = require('fs');
 import GUN from "gun";
 
 
 var WebSocketServer = require('ws').Server;
 
 export default function CNS() { 
-  var wss = new WebSocketServer({ port: 3006 });
+  var options = {
+    key:fs.readFileSync('./ssl/Nerves.key'),
+    cert:fs.readFileSync('./ssl/Nerves_cert.pem')
+  };
+  
+  var server = https.createServer(
+    options
+    , function(req,res) {
+      res.writeHeader(200);
+      res.end();
+    }
+  );
+  
+  server.listen(3006);
+
+
+  var wss = new ws({httpServer:server});
 
   console.log(`CNS Online`)
 
