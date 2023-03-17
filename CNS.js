5db3c1e66e87d05df4fb6a11b5eb9ad990f667fa
Daniel Tucker <DanielATucker@Outlook.com>
2023-03-17 08:43:50 -0400

    T

--- CNS.js
+++ CNS.js
@@ -9,6 +9,12 @@ export default function CNS() {
 
   let startServer = function() {
     let gun = GUN();
+
+    alice = gun.get('alice').put({name: 'alice', age: 22});
+
+    alice.on(function(node){
+      console.log('Subscribed to Alice!', node);
+    });
   }
 
   startServer();
